6fe4ad8f2ce3134fefbba0323f35dd44d0be3509
jeff <jeff.salarium@gmail.com>
2022-07-16 17:16:46 +0800

    user login

--- server/src/v1/controllers/user.js
+++ server/src/v1/controllers/user.js
@@ -19,6 +19,52 @@ exports.reqister = async (req, res) => {
       { expiresIn: "24h" }
     );
 
+    res.status(201).json({ user, token });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+exports.login = async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const user = await User.findOne({ username }).select("password username");
+    if (!user) {
+      return res.status(401).json({
+        errors: [
+          {
+            param: "username",
+            msg: "Invalid username or password",
+          },
+        ],
+      });
+    }
+
+    const decryptedPass = CryptoJS.AES.decrypt(
+      user.password,
+      process.env.PASSWORD_SECRET_KEY
+    ).toString(CryptoJS.enc.Utf8);
+
+    if (decryptedPass !== password) {
+      return res.status(401).json({
+        errors: [
+          {
+            param: "username",
+            msg: "Invalid username or password",
+          },
+        ],
+      });
+    }
+
+    user.password = undefined;
+
+    const token = jsonwebtoken.sign(
+      { id: user._id },
+      process.env.TOKEN_SECRET_KEY,
+      { expiresIn: "24h" }
+    );
+
     res.status(200).json({ user, token });
   } catch (err) {
     res.status(500).json(err);
