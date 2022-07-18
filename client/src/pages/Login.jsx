e71612e4e78d4f88eaf36254a756cf678e018133
jeff <jeff.salarium@gmail.com>
2022-07-19 02:00:03 +0800

    login page layout

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,7 +1,60 @@
-import React from "react";
+import { Box, TextField, Button } from "@mui/material";
+import LoadingButton from "@mui/lab/LoadingButton";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Login = () => {
-  return <div>Login</div>;
+  const [loading, setLoading] = useState(false);
+  const handleSubmit = () => {};
+  return (
+    <>
+      <Box
+        component="form"
+        sx={{
+          mt: 1,
+        }}
+        onSubmit={handleSubmit}
+      >
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          id="username"
+          label="Username"
+          name="username"
+          disabled={loading}
+        />
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          id="password"
+          label="Password"
+          name="password"
+          type="password"
+          disabled={loading}
+        />
+        <LoadingButton
+          sx={{ mt: 3, mb: 2 }}
+          variant="outlined"
+          fullWidth
+          color="success"
+          loading={loading}
+        >
+          Login
+        </LoadingButton>
+      </Box>
+      <Button
+        component={Link}
+        to="/signup"
+        sx={{
+          textTransform: "none",
+        }}
+      >
+        Don't have an account? Signup
+      </Button>
+    </>
+  );
 };
 
 export default Login;
